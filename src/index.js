ab153595b472df4f16bd932387e4fbc8dae7c15f
Antônio <aecf12@hotmail.com>
2024-10-31 16:45:55 -0300

    create routes api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
+import router  from "./routes";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(router);
 
-app.get("/doctors", (req,res) => {
-    res.status(200).send("Lista de médicos");
-});
+
 
 app.listen(3001, () => {
     console.log("Servidor on! :D")
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,9 @@
 import { Router} from "express";
 
 const router = Router();
+
+router.get("/doctors", (req,res) => {
+    res.status(200).send("Lista de médicos");
+});
+
+export default router; 
