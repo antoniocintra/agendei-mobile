36396442383a7697a875ad30beb2e4c48a442f3c
Antônio <aecf12@hotmail.com>
2024-10-31 22:24:14 -0300

    add feat list doctors

---
+++ src/services/service.doctor.js
@@ -0,0 +1,14 @@
+
+
+
+async function Listar() {
+    const doctors = [{id: 1, name: "Joao", specialty: "Cardiologista", icon: "M"},
+                     {id: 2, name: "Antonio", specialty: "Clinico Geral", icon: "M"},
+                     {id: 3, name: "Maria", specialty: "Cardiologista", icon: "F"}
+                    ];
+    return doctors;
+}
+
+
+
+export default {Listar}
